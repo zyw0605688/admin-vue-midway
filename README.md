2648403ed80f17900a77ab349c3c17fb2ababf1e
zyw0605688 <zyw0605688@163.com>
2023-12-18 15:53:57 +0800

    feat:'初始化'

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# midway-vue-admin
+# admin-vue-midway
+
+
+
+前端vue3+vite+element-plus
+
+后端midway+koa3
+
+
+
+图形化定义数据表，自动生curd接口和页面
---
+++ admin-midway/src/configuration.ts
@@ -0,0 +1,34 @@
+import { Configuration, App } from '@midwayjs/core';
+import * as koa from '@midwayjs/koa';
+import * as validate from '@midwayjs/validate';
+import * as info from '@midwayjs/info';
+// import { DefaultErrorFilter } from './filter/default.filter.js';
+// import { NotFoundFilter } from './filter/notfound.filter.js';
+import { ReportMiddleware } from './middleware/report.middleware.js';
+import DefaultConfig from './config/config.default.js';
+import UnittestConfig from './config/config.unittest.js';
+
+@Configuration({
+  imports: [
+    koa,
+    validate,
+    {
+      component: info,
+      enabledEnvironment: ['local'],
+    },
+  ],
+  importConfigs: [
+    {
+      default: DefaultConfig,
+      unittest: UnittestConfig,
+    },
+  ],
+})
+export class MainConfiguration {
+  @App('koa')
+  app: koa.Application;
+
+  async onReady() {
+    this.app.useMiddleware([ReportMiddleware]);
+  }
+}
